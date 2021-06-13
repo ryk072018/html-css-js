7989cb8e0ec32d9b8e596b832ca11815b2304cf2
ryk <ryk2100@163.com>
2021-06-14 01:16:42 +0800

    add flex function

--- JS/flexTwo.js
+++ JS/flexTwo.js
@@ -5,7 +5,6 @@ const selWra = document.querySelector(".sel-wra");
 const mainTwo = document.querySelector(".main-ctwo");
 
 // flex-grow
-const mainThree = document.querySelector(".main-cThree");
 const mainThreeA = document.querySelector(".three-a");
 const mainThreeB = document.querySelector(".three-b");
 const mainThreeC = document.querySelector(".three-c");
@@ -38,3 +37,78 @@ const changeGc = () => {
 numA.addEventListener("change", changeGa, true);
 numB.addEventListener("change", changeGb, true);
 numC.addEventListener("change", changeGc, true);
+
+// flex-shrink
+const fourA = document.querySelector(".four-a");
+const fourB = document.querySelector(".four-b");
+const fourC = document.querySelector(".four-c");
+const fouNumA = document.querySelector(".fouNumA");
+const fouNumB = document.querySelector(".fouNumB");
+const fouNumC = document.querySelector(".fouNumC");
+
+const changeShrinkA = () => {
+  fourA.style.flexShrink = fouNumA.value;
+};
+const changeShrinkB = () => {
+  fourB.style.flexShrink = fouNumB.value;
+};
+const changeShrinkC = () => {
+  fourC.style.flexShrink = fouNumC.value;
+};
+
+fouNumA.addEventListener("change", changeShrinkA, true);
+fouNumB.addEventListener("change", changeShrinkB, true);
+fouNumC.addEventListener("change", changeShrinkC, true);
+
+// flex-basis
+const fiveA = document.querySelector(".five-a");
+const fiveB = document.querySelector(".five-b");
+const fiveC = document.querySelector(".five-c");
+const fiveNumA = document.querySelector(".fiveNumA");
+const fiveNumB = document.querySelector(".fiveNumB");
+const fiveNumC = document.querySelector(".fiveNumC");
+
+const changeBasisA = () => {
+  fiveA.style.flexBasis = fiveNumA.value + "%";
+};
+const changeBasisB = () => {
+  fiveB.style.flexBasis = fiveNumB.value + "%";
+};
+const changeBasisC = () => {
+  fiveC.style.flexBasis = fiveNumC.value + "%";
+};
+
+fiveNumA.addEventListener("change", changeBasisA, true);
+fiveNumB.addEventListener("change", changeBasisB, true);
+fiveNumC.addEventListener("change", changeBasisC, true);
+
+// flex
+const sixA = document.querySelector(".six-a");
+const sixB = document.querySelector(".six-b");
+const sixC = document.querySelector(".six-c");
+const sixList = document.querySelectorAll(".six-item");
+
+const sixNumA1 = document.querySelector(".sixNumA1");
+const sixNumA2 = document.querySelector(".sixNumA2");
+const sixNumA3 = document.querySelector(".sixNumA3");
+
+const sixNumABC1 = document.querySelector(".sixNumABC1");
+const sixNumABC2 = document.querySelector(".sixNumABC2");
+const sixNumABC3 = document.querySelector(".sixNumABC3");
+
+const changeA = () => {
+  sixA.style.flex = `${sixNumA1.value} ${sixNumA2.value} ${sixNumA3.value}`;
+};
+const changeABC = () => {
+  sixList.forEach((item) => {
+    item.style.flex = `${sixNumABC1.value} ${sixNumABC2.value} ${sixNumABC3.value}`;
+  });
+};
+
+sixNumA1.addEventListener("change", changeA, true);
+sixNumA2.addEventListener("change", changeA, true);
+sixNumA3.addEventListener("change", changeA, true);
+
+sixNumABC1.addEventListener("change", changeABC, true);
+sixNumABC2.addEventListener("change", changeABC, true);
+sixNumABC3.addEventListener("change", changeABC, true);
